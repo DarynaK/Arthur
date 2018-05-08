a7968e64aee289538c45c7c11bb7360e93ba9463
DarynaK <dara.klymenko@gmail.com>
2018-05-08 14:06:23 +0300

    burger animation

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $('.burger').click(function() {
+    $(this).toggleClass('on');
     $('.burger-menu').toggleClass('active');
     });
     $('.burger-menu li').click(function() {
+      $('.burger').removeClass('on');
       $('.burger-menu').removeClass('active');
       });
 });
