f2f230ecd4698bb0f7d07ec25dc9f9d068b5601a
DarynaK <dara.klymenko@gmail.com>
2018-05-10 22:17:48 +0300

    beautify

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-  $('.burger').click(function() {
-    $(this).toggleClass('on');
-    $('.burger-menu').toggleClass('active');
+$(document).ready(function () {
+    $('.burger').click(function () {
+        $(this).toggleClass('on');
+        $('.burger-menu').toggleClass('active');
     });
-    $('.burger-menu li').click(function() {
-      $('.burger').removeClass('on');
-      $('.burger-menu').removeClass('active');
-      });
+    $('.burger-menu li').click(function () {
+        $('.burger').removeClass('on');
+        $('.burger-menu').removeClass('active');
+    });
 });
